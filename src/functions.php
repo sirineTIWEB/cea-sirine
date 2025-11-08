7d8012f56fc0ea2a99aabd625feba4ab9f3b98a4
rine <sirine.elalami.22578@stu.he-ferrer.eu>
2025-11-08 15:28:26 +0100

    comme les autres

--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,12 @@
 <?php
 
 // relier style
-
 function ajouter_style() {
      wp_enqueue_style( 'monstyle', get_stylesheet_uri() );
 
 }
 add_action( 'wp_enqueue_scripts', 'ajouter_style', PHP_INT_MAX );
+
+// ajouter une zone de menu
+
+register_nav_menu( 'primary', __( 'Navigation Menu' ) );
--- src/header.php
+++ src/header.php
@@ -8,3 +8,10 @@
 </head>
 <body>
     
+<?php
+wp_nav_menu(array(
+  'container' => false, // Ne pas mettre de container
+  'menu_class' => 'navbar-nav fw-bold justify-content-end align-items-center flex-grow-1', // Ajouter des classes à <ul>
+  'menu_id' => 'navbar', // Ajouter un ID à <ul>
+));
+?>
